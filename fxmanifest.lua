41c9083a912b75796475b8d554172e92a1d5ca05
CallMeNC <58883731+CallMeNC@users.noreply.github.com>
2022-01-05 21:42:52 -0500

    Updated file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ game 'rdr3'
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'VORP Core | rubi216 | Artzalez'
-description 'Hunting script for VORP Core'
+author 'CallMeNC'
+description 'Horse selling script for VORP Core'
 version 'alpha'
 
 client_script {
