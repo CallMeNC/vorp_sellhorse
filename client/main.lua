5d88026b69708626508b493bc54740dcd474a411
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-01-31 20:04:34 +0000

    Update main.lua
    added function for DrawText -__-

--- client/main.lua
+++ client/main.lua
@@ -92,6 +92,16 @@ function sellAnimal() -- Selling horse function
     end
 end
 
+function DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
+    local str = CreateVarString(10, "LITERAL_STRING", str)
+    SetTextScale(w, h)
+    SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+	SetTextCentre(centre)
+    if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
+	Citizen.InvokeNative(0xADA9255D, 1);
+    DisplayText(str, x, y)
+end
+
 Citizen.CreateThread(function()
     while true do
         local sleep = true
@@ -100,7 +110,7 @@ Citizen.CreateThread(function()
             if Vdist(playerCoords, v.coords) <= v.radius then -- Checking distance between player and trainer
                 local job
                 sleep = false
-                drawTxt(v.pressToSell, 0.5, 0.9, 0.7, 0.7, 255, 255, 255, 255, true, true)
+                DrawTxt(v.pressToSell, 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true,true)
                 if IsControlJustPressed(2, 0xD9D0E1C0) then
                     if Config.joblocked then 
                         TriggerEvent('vorp:ExecuteServerCallBack','vorp_sellhorse:getjob', function(result)  
